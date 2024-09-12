ee2f36f2d7720be2945414d1c9744dbb206d084d
Praval <pravalp949@gmail.com>
2024-09-12 18:29:14 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,49 @@
+import "./../styles/App.css";
 
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
 
-const App = () => {
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [value, setValue] = useState("");
+
+  const handletask = () => {
+    if (value.trim()) {
+      setTodos([...todos, value]);
+      setValue("");
+    }
+  };
+  const handleDelete = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index); // Remove task by index
+    setTodos(newTodos);
+  };
+
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <>
+      <div className="main">
+        <h3>Todo app</h3>
+        <input
+          type="text"
+          placeholder="Enter a task"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button onClick={handletask}>Add Task</button>
+      </div>
+      <div className="todo">
+        <ul>
+          {Array.isArray(todos) &&
+            todos.map((todo, index) => (
+              <li key={index}>
+                {todo}
+                <button onClick={() => handleDelete(index)} className="btn">
+                  Delete
+                </button>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
